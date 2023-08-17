b800959faf0b1d30db681de14b038fa5ef50b72d
Matias Ikaczijk <matiasnicolasika@gmail.com>
2023-08-17 16:20:51 -0300

    Terminamos con los ciclos While, do while y for de la clase 1 en java

--- JavaSCuatri/Leccion2/Clase1.java
+++ JavaSCuatri/Leccion2/Clase1.java
@@ -2,9 +2,27 @@ package JavaSCuatri.Leccion2;
 public class Clase1{
     public static void main(String[] args) {
         var conteo = 0; // inf de tipo
-        while (conteo < 7) {
+        // CICLO WHILE
+        while (conteo < 7) { // si esta condicion no se cumple, no podremos ver el codigo del prog
             System.out.println("Conteo = " + conteo);
             conteo++; // va incrementando uno
+
+            // ciclo Do WHILE: Por lo menos una vez se ejecuta el ciclo
+            var contador = 0;
+            do{
+                System.out.println("contador = " + contador);
+                contador++;
+            }while (contador <= 7);
+
+            // 1er espacio se declara la variable, 2do condicion a cumplir, 3ro: incremento o decremento de la var(recorrido)
+            for( var cont = 0 ; cont < 7 ; cont++) {
+                System.out.println(" cont = " + cont);
+            }
+
+
+
+
+
 
         }
     }
