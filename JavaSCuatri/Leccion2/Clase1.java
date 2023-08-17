2f2d2d363e48b2ff060e0f7cecf2db515c4ac196
Matias Ikaczijk <matiasnicolasika@gmail.com>
2023-08-17 15:33:49 -0300

    Agregamos la carpeta nueva con la primer clase

---
+++ JavaSCuatri/Leccion2/Clase1.java
@@ -0,0 +1,11 @@
+package JavaSCuatri.Leccion2;
+public class Clase1{
+    public static void main(String[] args) {
+        var conteo = 0; // inf de tipo
+        while (conteo < 7) {
+            System.out.println("Conteo = " + conteo);
+            conteo++; // va incrementando uno
+
+        }
+    }
+}
