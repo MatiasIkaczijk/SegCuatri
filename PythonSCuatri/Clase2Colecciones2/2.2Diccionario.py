f3ba614179593c81361984a19a3eb6a7b8baf1d7
Matias Ikaczijk <matiasnicolasika@gmail.com>
2023-08-28 23:36:46 -0300

    Cargamos modificaciones

---
+++ PythonSCuatri/Clase2Colecciones2/2.2Diccionario.py
@@ -0,0 +1,20 @@
+# ¿Qué es un diccionario? coleccion de datos ordenados de manera particular
+
+# "Maradona" : 10 --> el conjunto de datos es un diccionario
+
+# UNA LLAVE Y UN VALOR
+# dict(key,value)
+
+diccionario = { 
+    "IDE" : "Integrated Development Environment",
+    "POO" : "Programacion Orientada a Objetos",
+    "SABD" : "Sistema de Administracion de Base de Base de Datos"
+}
+# mostramos el diccionario con tres elementos
+print(diccionario)
+
+#verificar cant de elementos
+print(len(diccionario))
+
+# como acceder al diccionario con la llave (key)
+print(diccionario["IDE"])
