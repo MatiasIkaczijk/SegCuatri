ceaf18152b7eca5effea9f4c416458c7a01f3d34
Matias Ikaczijk <matiasnicolasika@gmail.com>
2023-08-29 11:33:51 -0300

    Terminamos con la segunda clase de Python

--- PythonSCuatri/Clase2Colecciones2/2.2Diccionario.py
+++ PythonSCuatri/Clase2Colecciones2/2.2Diccionario.py
@@ -26,3 +26,36 @@ print(diccionario.get("SABD"))
 # modificar un elemento 
 diccionario["IDE"] = " Entorno de Desarrollo Integrado"
 print(diccionario)
+
+# ¿cómo recorrer los elementos?
+for termino in diccionario:
+    print(termino)
+# necesitamos una funcionpara recorrer un diccionario
+for termino, valor in diccionario.items():
+    print(termino, valor)
+
+# otra manera de acceder al diccionario.
+for termino in diccionario.keys():
+    print(termino) # muestra solo las llaves
+
+for valor in diccionario.values(): # muestra los valores sin las llaves
+    print(valor)
+
+# comprobar si una llave está o no está en el diccionario 
+print("IDE" in diccionario) #devuelve un booleano (v o f)
+
+# agregar un elemento 
+diccionario["PK"] = "Primary key"
+print(diccionario)
+
+# eliminar elementos
+diccionario.pop("SABD")
+print(diccionario)
+
+# vaciar un diccionario 
+diccionario.clear()
+print(diccionario)
+
+# eliminar diccionario 
+del diccionario
+print(diccionario)
