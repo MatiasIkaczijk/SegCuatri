cdc3f52becd608e803edbfc2ded60936ed618352
Matias Ikaczijk <matiasnicolasika@gmail.com>
2023-09-20 18:05:11 -0300

    Cargamos mas ejercicios de python en la clase 5

---
+++ PythonSCuatri/Clase 5/Ejercicio7.py
@@ -0,0 +1,7 @@
+# Adivinar el numero:
+
+# Realizar un juego para adivinar el numero. Para ello se debe generar
+# un numero aleat entre 1 - 100 y luego ir pidiendo numero indicando
+# si es mayor o menor segun N. El proceso termina
+# cuando el usuario acierta y se muestra la cantidad de intentos
+
