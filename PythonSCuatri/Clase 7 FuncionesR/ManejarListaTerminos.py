8ade967d36e8c4d616a56a0b8c80451af2ead4e0
Matias Ikaczijk <matiasnicolasika@gmail.com>
2023-10-02 21:23:53 -0300

    Cargando ejercicios y funciones recursivas..

--- PythonSCuatri/Clase 7 FuncionesR/ManejarListaTerminos.py
+++ PythonSCuatri/Clase 7 FuncionesR/ManejarListaTerminos.py
@@ -8,3 +8,18 @@ ListaTerminos(Nombre = "Leo Messi")
 #                 1: Recibe arg
 #                 2: Convierte en paramentros
 #                 3: Hace la operacion y muestra el diccionario
+
+
+# Distintos tipos de datos, como argumento:
+# funcion que recibe lista con elementos:
+
+def MostrarNombres(nombres):
+    for nombre in nombres:
+        print(nombre)
+nombres2 = ["Juan", "Coscu", "Oscar"] # entra, asigna nombre = Juan, vuelve a entrar nombre = Coscu...
+MostrarNombres(nombres2)
+MostrarNombres("Jaz")
+
+# MostrarNombres(10,11) No es iterable... pero si ponemos:
+# MostrarNombres((10,11)) se convierte en tupla, es decir iterable y puede recorrerlo
+# MostrarNombres ([10,11]) lo convertimos a lista
