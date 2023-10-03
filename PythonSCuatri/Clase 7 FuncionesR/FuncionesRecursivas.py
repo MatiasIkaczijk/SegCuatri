c19771fe5d01a7f28c12b92b85ddb7beb6b9a882
Matias Ikaczijk <matiasnicolasika@gmail.com>
2023-10-02 22:40:22 -0300

    Terminamos con la clase de Python y ejercicios de grados..

--- PythonSCuatri/Clase 7 FuncionesR/FuncionesRecursivas.py
+++ PythonSCuatri/Clase 7 FuncionesR/FuncionesRecursivas.py
@@ -6,7 +6,8 @@ def factorial(numero): # recibimos num, envia arg, recibimos como parametro y nu
         return 1 # caso base
     else:
         return numero * factorial(numero - 1) # caso recursivo
-resultado = factorial(5) # codigo duro
-print(f" El factorial del numero 5 es: {resultado}")
+numFactorial = int(input("Escriba un numero para calcular su factorial: "))
+resultado = factorial(numFactorial) # codigo duro
+print(f" El factorial del número {numFactorial} es: {resultado}")
 
 # Para hacer la tarea q nos pide. Haria una entrada de info con un input, solicitando q ingrese un numero
