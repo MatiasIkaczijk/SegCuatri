083a37449abd23119f30b3e0423e0ff063980f42
Matias Ikaczijk <matiasnicolasika@gmail.com>
2023-10-09 22:41:58 -0300

    Trabajando en la clase 8 de JS

---
+++ JavaScriptSCuatri/Clase8JS/Ejercicio.js
@@ -0,0 +1,12 @@
+// 8.1 Ejercicio: dentro de un rango: Operador and
+// operador and -> && Doble aspersan
+
+let dentroDeRango = -10;
+let valMinimo = 0, valMax = 10;
+
+if(dentroDeRango >= valMinimo && dentroDeRango <= valMax){
+    console.log("El numero está dentro del rango")
+}
+else{
+    console.log("El número ingresado está fuera del rango")
+}
