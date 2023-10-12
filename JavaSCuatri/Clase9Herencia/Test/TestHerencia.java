580d2d2493c475aea722e67e3ecdfc64ffd2238c
Matias Ikaczijk <matiasnicolasika@gmail.com>
2023-10-11 23:38:50 -0300

    Seguimos trabajando con java

--- JavaSCuatri/Clase9Herencia/Test/TestHerencia.java
+++ JavaSCuatri/Clase9Herencia/Test/TestHerencia.java
@@ -4,10 +4,10 @@ import Clase9Herencia.Domain.Empleado;
 
 public class TestHerencia {
     // lo que sucede cuando se llama a la class empleado, ve que está vacia
-    // pero al ser hija de la class Persona, va y muestra los atributos vacios..
-    
+    // pero al ser hija de la class Persona, va y muestra los atributos vacios.. del toString
+
     public static void main(String[] args) {
-        Empleado empleado1 = new Empleado();
+        Empleado empleado1 = new Empleado("Matias", 58000.0);
         System.out.println("Empleado1 = " + empleado1);
     }
 }
