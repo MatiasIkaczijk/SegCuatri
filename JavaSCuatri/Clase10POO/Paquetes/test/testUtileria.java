763891ca8713ece684c0efc205bd76cf040884bf
Matias Ikaczijk <matiasnicolasika@gmail.com>
2023-10-18 23:08:41 -0300

    Creacion de paquetes, utileria, test utilieria

---
+++ JavaSCuatri/Clase10POO/Paquetes/test/testUtileria.java
@@ -0,0 +1,9 @@
+package Clase10POO.Paquetes.test;
+
+import Clase10POO.Paquetes.Codesystem.Utileria;
+
+public class testUtileria {
+    public static void main(String[] args) {
+        Utileria.imprimir("Hola amigos");
+    }
+}
