6bf5eb5fd2dc3aed569f13afa7edde4d30bd126a
Matias Ikaczijk <matiasnicolasika@gmail.com>
2023-10-25 23:32:50 -0300

    Comenzamos a realizar los ejercicios con arreglos

--- JavaSCuatri/Clase11Arreglos/Test/TestArreglosObject.java
+++ JavaSCuatri/Clase11Arreglos/Test/TestArreglosObject.java
@@ -12,6 +12,16 @@ public class TestArreglosObject {
         System.out.println("personas 0 = " + personas[0]);
         System.out.println("personas 1 = " + personas[1]);
         
+        // Iteracion 
+        for(int i = 0; i < personas.length; i++){
+            System.out.println("personas " + i + " = " + personas[i]);
+        }
 
+        // Arrreglos de sintesis resumida
+
+        String frutas[] = {"Banana", "Pera", "Tomate"};
+        for(int i = 0; i < frutas.length; i++){
+            System.out.println("Frutas = " + frutas[i]);
+        }
     }
 }
